a5e0bf1b3bd36f64378ded8398a53c25cf24b24f
AbhishekD <darekarabhishek@gmail.com>
2022-08-03 21:30:38 +0530

    Added line in Main branch

--- file1.py
+++ file1.py
@@ -1,12 +1,6 @@
 
 def main():
-    print("This is File 1")
-    print("This is the extra added line")
-    print("Extra line added from Dev")
-
-
-    print("Added in main branch for conflict")
-    print("File added in Test for conflict")
+    print("This is main Branch")
 
 if __name__== "__main__":
     main()
--- file_test.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    print("This is File Added from test branch")
-    print("Changed this in Main branch")
-
-
-if __name__== "__main__":
-    main()
